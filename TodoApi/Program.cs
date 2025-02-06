67e624e5e5eca1b33101acf7b374142590d5885f
chaya-kanyevski <c0533196732@gmail.com>
2025-02-06 15:24:02 +0200

    Initial commit

---
+++ TodoApi/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("ToDoDB"),
+        new MySqlServerVersion(new Version(8, 0, 0))
+    ));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoList API");
+    c.RoutePrefix = string.Empty;
+});
+app.UseCors();
+
+app.MapGet("/", () => "TodoList API works...");
+
+app.MapGet("/items", async (ToDoDbContext db) =>
+{
+    var items = await db.Items.ToListAsync();
+    return Results.Ok(items);
+});
+
+app.MapGet("/items/{id}", async (int id, ToDoDbContext db) =>
+    await db.Items.FindAsync(id) is Item item ? Results.Ok(item) : Results.NotFound());
+
+app.MapPost("/", async (Item item, ToDoDbContext db) => {
+    db.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/items/{item.Id}", item);
+});
+
+app.MapPut("/items/{id}", async (int id, bool IsComplete, ToDoDbContext db) => {
+    var item = await db.Items.FindAsync(id);
+    if (item is null) return Results.NotFound();
+
+    item.IsComplete = IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/items/{id}", async (int id, ToDoDbContext db) => {
+    var item = await db.Items.FindAsync(id);
+    if(item is null) return Results.NotFound();
+
+    db.Items.Remove(item);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.Run();
