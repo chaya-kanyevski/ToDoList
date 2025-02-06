82377e4c736e968a214f6fb85aa7af2c3a0f6955
chaya-kanyevski <c0533196732@gmail.com>
2025-02-06 15:48:17 +0200

    Add ToDoListReact

---
+++ ToDoListReact/src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+axios.defaults.baseURL =process.env.REACT_APP_API_URL;
+
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+      console.error('API Error:', error.response ? error.response.data : error.message);
+      return Promise.reject(error); 
+    }
+  );
+
+export default axios;
