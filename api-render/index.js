2d9feb5ae0bb087afb4a6af8aa1aa5afc87cd2af
chaya-kanyevski <c0533196732@gmail.com>
2025-02-09 00:21:15 +0200

    adding api-render project

---
+++ api-render/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.get('/', async (req, res) => {
+  try {
+    const apiKey = process.env.RENDER_API_KEY;
+
+    const response = await axios.get('https://api.render.com/v1/services', {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching data from Render API:', error);
+    res.status(500).json({ message: 'Error fetching data' });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
